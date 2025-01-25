a16d341f4cdc9f944b5a8925d572cd297b9d7a4a
Cyrus Majidy <cyrus_maj@yahoo.com>
2025-01-25 16:51:21 -0500

    Added Authentication with Auth0 and User UI

---
+++ Models/UserProfileViewModel.cs
@@ -0,0 +1,7 @@
+namespace SchoolManagementApp.MVC.Models;
+    public class UserProfileViewModel
+    {
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string ProfileImage { get; set; }
+    }
